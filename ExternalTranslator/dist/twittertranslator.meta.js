3fb52ec20e37eda2264c44807b6f4194b842ab05
MagicOfLolis <magicoflolis@tuta.io>
2021-12-19 17:29:42 -0800

    [TET] v.0.29a1
    Signed-off-by: MagicOfLolis <magicoflolis@tuta.io>

---
+++ ExternalTranslator/dist/twittertranslator.meta.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         [Dev] Twitter External Translator
+// @description  Adds external & internal translators
+// @version      12.19.21
+// @author       Magic <magicoflolis@tuta.io>
+// @icon         https://abs.twimg.com/favicons/twitter.ico
+// @namespace    https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator#twitter-external-translator
+// @homepageURL  https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator#twitter-external-translator
+// @supportURL   https://github.com/magicoflolis/userscriptrepo/issues/new
+// @downloadURL  https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator/dist/twittertranslator.user.js
+// @updateURL    https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator/dist/twittertranslator.meta.js
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js?_=1639963678499
+// @match        https://mobile.twitter.com/*
+// @match        https://twitter.com/*
+// @match        https://tweetdeck.twitter.com/*
+// @match        https://www.twitlonger.com/show/*
+// @match        https://nitter.net/*
+// @match        https://nitter.*/*
+// @match        https://nitter.*.*/*
+// @match        https://nitter.domain.glass/*
+// @match        https://nitter-home.kavin.rocks/*
+// @match        https://birdsite.xanny.family/*
+// @match        https://twitr.gq/*
+// @exclude      https://twitter.com/login
+// @exclude      https://twitter.com/signup
+// @exclude      https://twitter.com/i/flow/signup
+// @exclude      https://twitter.com/teams/*
+// @exclude      https://twitter.com/*/authorize?*
+// @exclude      https://twitter.com/*/begin_password_reset
+// @grant        GM_getValue
+// @grant        GM.getValue
+// @grant        GM_setValue
+// @grant        GM.setValue
+// @grant        GM_deleteValue
+// @grant        GM.deleteValue
+// @grant        GM_info
+// @grant        GM_xmlhttpRequest
+// @license      MIT
+// ==/UserScript==
