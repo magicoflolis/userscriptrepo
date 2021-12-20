8b27a2d1450a4985c9859927e2b0ec88e8e68701
MagicOfLolis <magicoflolis@tuta.io>
2021-12-19 17:39:07 -0800

    [TET] v0.30
    Signed-off-by: MagicOfLolis <magicoflolis@tuta.io>

--- ExternalTranslator/dist/twittertranslator.user.js
+++ ExternalTranslator/dist/twittertranslator.user.js
@@ -55,15 +55,13 @@
 // @description:ru-RU   Добавляет сторонних переводчиков в Twitter
 // @description:ru      Добавляет сторонних переводчиков в Twitter
 // @description:es      Añade traductores de terceros a Twitter
-// @version      0.29c1
+// @version      0.30
 // @author       Magic <magicoflolis@tuta.io>
 // @icon         https://abs.twimg.com/favicons/twitter.ico
 // @namespace    https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator#twitter-external-translator
 // @homepageURL  https://github.com/magicoflolis/userscriptrepo/tree/master/ExternalTranslator#twitter-external-translator
 // @supportURL   https://github.com/magicoflolis/userscriptrepo/issues/new
-// @downloadURL  https://github.com/magicoflolis/userscriptrepo/raw/master/ExternalTranslator/dist/twittertranslator.user.js
-// @updateURL    https://github.com/magicoflolis/userscriptrepo/raw/master/ExternalTranslator/dist/twittertranslator.meta.js
-// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js?_=1639964171650
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js?_=1639964308532
 // @match        https://mobile.twitter.com/*
 // @match        https://twitter.com/*
 // @match        https://tweetdeck.twitter.com/*
